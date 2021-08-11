bc0e0628f0cc1c0fa591946d6b1111be37b37fa2
PapaBaGaye <pba.gaye@univ-thies.sn>
2021-08-11 21:44:08 +0000

    Mise à jour des fichier  .py

--- main.py
+++ main.py
@@ -11,9 +11,15 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        c = factorial(n) / (factorial(k) * factorial(n-k))
-        x = c * p ** k * (1-p) **(n-k)
-        return x
+        if (n >= k) :#or (p >= 0 and p <= 1)
+            if (p >= 0 and p <= 1):
+                c = factorial(n) / (factorial(k) * factorial(n-k))
+                x = c * p ** k * (1-p) **(n-k)
+                return x
+            else:
+                return 'p doit etre compris entre 0 et 1'
+        elif (n < k) :#or (p < 0 and p >= 1)
+            return 'Erreur : k doit etre inferieur à n'
 
     def repartitionBinomiale(self, n, p, k):
         """
@@ -25,10 +31,16 @@ class Binomial:
         entrées :
             - densiteBinomiale(self, n, p, 0)
         """
-        S = self.densiteBinomiale(n, p, 0)
-        for i in range(k):
-            S = S + self.densiteBinomiale(n, p, i+1)
-        return S
+        if (n >= k) :#or (p >= 0 and p <= 1)
+            if (p >= 0 and p <= 1):
+                S = self.densiteBinomiale(n, p, 0)
+                for i in range(k):
+                    S = S + self.densiteBinomiale(n, p, i+1)
+                return S
+            else:
+                return 'p doit etre compris entre 0 et 1'
+        elif (n < k) :#or (p < 0 and p >= 1)
+            return 'Erreur : k doit etre inferieur à n'
 
     def repartitionBinomialeNegative(self, n, p, k):
         """
@@ -40,10 +52,17 @@ class Binomial:
         entrées :
             - densiteBinomiale(self, n, p, 0)
         """
-        S = self.densiteBinomialeNegative(n, p, 0)
-        for i in range(k):
-            S = S + self.densiteBinomialeNegative(n, p, i+1)
-        return S
+        if (n >= k) :#or (p >= 0 and p <= 1)
+            if (p >= 0 and p <= 1):
+                S = self.densiteBinomialeNegative(n, p, 0)
+                for i in range(k):
+                    S = S + self.densiteBinomialeNegative(n, p, i+1)
+                return S
+            else:
+                return 'p doit etre compris entre 0 et 1'
+
+        elif (n < k) :#or (p < 0 and p >= 1)
+            return 'Erreur : k doit etre inferieur à n'
 
     def densiteBinomialeNegative(self, n, p, k):
         """
@@ -52,9 +71,15 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        c = factorial(k+n-1) / (factorial(k) * factorial(n-1))
-        x = c * p ** k * (1-p) **(n-k)
-        return x
+        if (n >= k) :#or (p >= 0 and p <= 1)
+            if (p >= 0 and p <= 1):
+                c = factorial(k+n-1) / (factorial(k) * factorial(n-1))
+                x = c * p ** k * (1-p) **(n-k)
+                return x
+            else:
+                return 'p doit etre compris entre 0 et 1'
+        elif (n < k) :#or (p < 0 and p >= 1)
+            return 'Erreur : k doit etre inferieur à n'
 
     def esperanceBinomial(self, n, p):
         """
@@ -62,8 +87,11 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        esp = n * p
-        return esp
+        if (p >= 0 and p <= 1):
+            esp = n * p
+            return esp
+        else:
+            return 'p doit etre compris entre 0 et 1'
 
     def varianceBinomial(self, n, p):
         """
@@ -71,8 +99,11 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        var = n * p * (1 - p)
-        return var
+        if (p >= 0 and p <= 1):
+            var = n * p * (1 - p)
+            return var
+        else:
+            return 'p doit etre compris entre 0 et 1'
 
     def esperanceBinomialeNegative(self, n, p):
         """
@@ -80,8 +111,11 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        esp = (n * (p - 1)) / p
-        return esp
+        if (p >= 0 and p <= 1):
+            esp = (n * (p - 1)) / p
+            return esp
+        else:
+            return 'p doit etre compris entre 0 et 1'
 
     def varianceBinomialeNegative(self, n, p):
         """
@@ -89,5 +123,8 @@ class Binomial:
             - n : nombre d'epreuves
             - p : probabilité
         """
-        var = (n * (p - 1)) / p**2
-        return var
+        if (p >= 0 and p <= 1):
+            var = (n * (p - 1)) / p**2
+            return var
+        else:
+            return 'p doit etre compris entre 0 et 1'
